e1e2f9c9d0fcf53a404bbff152a7cf7540bedb1c
devedbox <devedbox@gmail.com>
2017-04-19 16:15:28 +0800

    Added SessionDelegate.swift file.

---
+++ Source/SessionDelegate.swift
@@ -0,0 +1,13 @@
+//
+//  SessionDelegate.swift
+//  AxziplinNet
+//
+//  Created by devedbox on 2017/4/19.
+//  Copyright © 2017年 devedbox. All rights reserved.
+//
+
+import Foundation
+
+class SessionDelegate: NSObject {
+
+}
