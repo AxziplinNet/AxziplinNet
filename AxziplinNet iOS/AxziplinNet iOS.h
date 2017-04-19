066d221dd55c645689e10ecfff9068b6e97b0168
devedbox <862099730@qq.com>
2017-04-19 21:48:24 +0800

    Added target AxziplinNet iOS.

---
+++ AxziplinNet iOS/AxziplinNet iOS.h
@@ -0,0 +1,19 @@
+//
+//  AxziplinNet iOS.h
+//  AxziplinNet iOS
+//
+//  Created by devedbox on 2017/4/19.
+//  Copyright © 2017年 devedbox. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for AxziplinNet iOS.
+FOUNDATION_EXPORT double AxziplinNet_iOSVersionNumber;
+
+//! Project version string for AxziplinNet iOS.
+FOUNDATION_EXPORT const unsigned char AxziplinNet_iOSVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <AxziplinNet_iOS/PublicHeader.h>
+
+
