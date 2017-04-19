2068cee29ae714c9f839e2fa07370d1194ba31c3
devedbox <devedbox@gmail.com>
2017-04-19 15:49:13 +0800

    Added AxziplinNet.swift file.

---
+++ Source/AxziplinNet.swift
@@ -0,0 +1,13 @@
+//
+//  AxziplinNet.swift
+//  AxziplinNet
+//
+//  Created by devedbox on 2017/4/19.
+//  Copyright © 2017年 devedbox. All rights reserved.
+//
+
+import Cocoa
+
+class AxziplinNet: NSObject {
+
+}
