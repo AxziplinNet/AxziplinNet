4dab0991a0f2d40d22fd1226001d817a8beabcbb
devedbox <devedbox@gmail.com>
2017-04-19 15:13:18 +0800

    Initial Commit

---
+++ AxziplinNet/AxziplinNet.h
@@ -0,0 +1,19 @@
+//
+//  AxziplinNet.h
+//  AxziplinNet
+//
+//  Created by devedbox on 2017/4/19.
+//  Copyright © 2017年 devedbox. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for AxziplinNet.
+FOUNDATION_EXPORT double AxziplinNetVersionNumber;
+
+//! Project version string for AxziplinNet.
+FOUNDATION_EXPORT const unsigned char AxziplinNetVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <AxziplinNet/PublicHeader.h>
+
+
