e74c4d53d2005046db1f354671b794d3554dda11
devedbox <devedbox@gmail.com>
2017-04-20 16:00:37 +0800

    Added HTTPMethod enum type.

--- Source/AxziplinNet.swift
+++ Source/AxziplinNet.swift
@@ -28,3 +28,17 @@ import Foundation
 class AxziplinNet: NSObject {
 
 }
+
+extension AxziplinNet {
+    public enum HTTPMethod: String {
+        case options = "OPTIONS"
+        case get     = "GET"
+        case head    = "HEAD"
+        case post    = "POST"
+        case put     = "PUT"
+        case patch   = "PATCH"
+        case delete  = "DELETE"
+        case trace   = "TRACE"
+        case connect = "CONNECT"
+    }
+}
