bd3051c0e24cbde44cf8cd55a0236021d88c5e1d
devedbox <devedbox@gmail.com>
2017-04-21 17:29:13 +0800

    Move HTTPMethod's definition to Request.

--- Source/AxziplinNet.swift
+++ Source/AxziplinNet.swift
@@ -30,17 +30,3 @@ public class AxziplinNet: NSObject {
         return nil
     }
 }
-
-extension AxziplinNet {
-    public enum HTTPMethod: String {
-        case options = "OPTIONS"
-        case get     = "GET"
-        case head    = "HEAD"
-        case post    = "POST"
-        case put     = "PUT"
-        case patch   = "PATCH"
-        case delete  = "DELETE"
-        case trace   = "TRACE"
-        case connect = "CONNECT"
-    }
-}
