6a23735f1b8b4dba5fd98abcc03ca1ffea6a021a
devedbox <devedbox@gmail.com>
2017-04-21 16:42:02 +0800

    Changed access control of escaped() to fileprivate.

--- Source/Request.swift
+++ Source/Request.swift
@@ -116,7 +116,7 @@ extension String {
     /// should be percent-escaped in the query string.
     ///
     /// - returns: The percent-escaped string.
-    private func escaped() throws -> String {
+    fileprivate func escaped() -> String {
         let generalDelimitersToEncode = ":#[]@" // does not include "?" or "/" due to RFC 3986 - Section 3.4
         let subDelimitersToEncode = "!$&'()*+,;="
         
