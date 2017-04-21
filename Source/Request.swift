bdfc0b0b9606eed7503c033e96b8bfb2dccf7ec8
devedbox <devedbox@gmail.com>
2017-04-21 16:34:22 +0800

    Added escaped string extension.

--- Source/Request.swift
+++ Source/Request.swift
@@ -81,3 +81,56 @@ extension String: URLConvertible {
         return url
     }
 }
+
+extension URL: URLConvertible {
+    public func asURL() throws -> URL { return self }
+}
+
+extension String {
+    /// Returns a percent-escaped string following RFC 3986 for a query string key or value.
+    ///
+    /// RFC 3986 states that the following characters are "reserved" characters.
+    ///
+    /// - General Delimiters: ":", "#", "[", "]", "@", "?", "/"
+    /// - Sub-Delimiters: "!", "$", "&", "'", "(", ")", "*", "+", ",", ";", "="
+    ///
+    /// In RFC 3986 - Section 3.4, it states that the "?" and "/" characters should not be escaped to allow
+    /// query strings to include a URL. Therefore, all "reserved" characters with the exception of "?" and "/"
+    /// should be percent-escaped in the query string.
+    ///
+    /// - returns: The percent-escaped string.
+    private func escaped() throws -> String {
+        let generalDelimitersToEncode = ":#[]@" // does not include "?" or "/" due to RFC 3986 - Section 3.4
+        let subDelimitersToEncode = "!$&'()*+,;="
+        
+        var allowedCharacterSet = CharacterSet.urlQueryAllowed
+        allowedCharacterSet.remove(charactersIn: "\(generalDelimitersToEncode)\(subDelimitersToEncode)")
+        
+        var escaped = ""
+        
+        //  Batching is required for escaping due to an internal bug in iOS 8.1 and 8.2. Encoding more than a few
+        //  hundred Chinese characters causes various malloc error crashes. To avoid this issue until iOS 8 is no
+        //  longer supported, batching MUST be used for encoding. This introduces roughly a 20% overhead.
+        
+        if #available(iOS 8.3, *) {
+            escaped = addingPercentEncoding(withAllowedCharacters: allowedCharacterSet) ?? self
+        } else {
+            let batchSize = 50
+            var index = startIndex
+            
+            while index != endIndex {
+                let startIndex = index
+                let _endIndex = self.index(index, offsetBy: batchSize, limitedBy: endIndex) ?? endIndex
+                let range = startIndex..<_endIndex
+                
+                let _substring = substring(with: range)
+                
+                escaped += _substring.addingPercentEncoding(withAllowedCharacters: allowedCharacterSet) ?? _substring
+                
+                index = _endIndex
+            }
+        }
+        
+        return escaped
+    }
+}
