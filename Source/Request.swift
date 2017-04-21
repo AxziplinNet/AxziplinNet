597ae79a061bace718f177aaad18fc65a59ef9ab
devedbox <devedbox@gmail.com>
2017-04-21 14:28:34 +0800

    Added Request-related types and protocols.

--- Source/Request.swift
+++ Source/Request.swift
@@ -26,10 +26,34 @@
 
 import Foundation
 
-public protocol Request {
+/// Types adopting the `URLRequestConvertible` protocol can be used to construct URL requests.
+public protocol URLRequestConvertible {
+    /// Returns a URL request or throws if an `Error` was encountered.
+    ///
+    /// - throws: An `Error` if the underlying `URLRequest` is `nil`.
+    ///
+    /// - returns: A URL request.
+    func asURLRequest() throws -> URLRequest
+}
+
+public protocol RequestResult {
     
 }
 
 public protocol RequestEncoding {
-    
+    func encode(_ request: URLRequestConvertible, with parameters: Request.RequestParameters) throws -> URLRequest?
+}
+
+public class Request {}
+
+extension Request {
+    public typealias RequestParameters = [String: Any]
+}
+
+extension URLRequestConvertible {
+    var request: URLRequest? { return try? asURLRequest() }
+}
+
+extension URLRequest: URLRequestConvertible {
+    public func asURLRequest() throws -> URLRequest { return self }
 }
